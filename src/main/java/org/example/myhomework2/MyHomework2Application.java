793700d96d7fbb44d39dab40f7b2722b52b92527
AllSungho <nanumeran@gmail.com>
2025-08-09 04:21:07 +0900

    first commit

---
+++ src/main/java/org/example/myhomework2/MyHomework2Application.java
@@ -0,0 +1,13 @@
+package org.example.myhomework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomework2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHomework2Application.class, args);
+    }
+
+}
